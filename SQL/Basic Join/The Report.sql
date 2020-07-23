f72e988fb2dbe93dca1ed78eb9dc5ebcceb16f63
kliu2018 <40738650+kliu2018@users.noreply.github.com>
2020-07-22 22:31:00 -0400

    Update The Report.sql

--- SQL/Basic Join/The Report.sql
+++ SQL/Basic Join/The Report.sql
@@ -1,9 +1,5 @@
-SELECT IF(Grades.Grade >8, Students.Name, NULL), Grades.Grade, Students.Marks
+SELECT IF(Grades.Grade >7, Students.Name, NULL), Grades.Grade, Students.Marks
 FROM Students
-Inner Join Grades
-ON Students.Marks >Grades.Min_Mark AND Students.Marks < Grades.Max_Mark 
-ORDER BY CASE Grades.Grade
-             WHEN  >8 THEN Grades.Grade END DESC, 
-             WHEN  >8 THEN Students.Name END,
-             WHEN <8 THEN Students.Marks END ASC, 
-             WHEN <8 THEN Students.Name END
+Join Grades
+ON Students.Marks BETWEEN Grades.Min_Mark AND Grades.Max_Mark 
+ORDER BY -Grades.Grade, Students.Name, Students.Marks
